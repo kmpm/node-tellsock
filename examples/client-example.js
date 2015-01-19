91fdacdbab9bf44712277d2f82096c744cb596ac
Peter Magnusson <peter@birchroad.net>
2015-01-19 22:37:53 +0100

    adde tdMethods to client

--- examples/client-example.js
+++ examples/client-example.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-
-var TelldusClient = require('..').TelldusClient;
+var tellsock = require('..');
+var TelldusClient = tellsock.TelldusClient;
 
 
 
@@ -9,11 +9,17 @@ var tc = new TelldusClient();
 
 tc.tdGetNumberOfDevices()
 .then(function (count) {
-  console.log('%d number of devices is configured');
+  console.log('%d number of devices is configured', count);
   return tc.tdGetName(1);
 })
 .then(function (name) {
   console.log('device 1 is called "%s"', name);
+  return tc.tdMethods(1, tellsock.DeviceMethods.TURNON | tellsock.DeviceMethods.TURNOFF);
+})
+.then(function (methods) {
+  console.log('supports turnon', (methods & tellsock.DeviceMethods.TURNON) > 0);
+  console.log('supports turnoff', (methods & tellsock.DeviceMethods.TURNOFF) > 0);
+  console.log('supports dim', (methods & tellsock.DeviceMethods.DIM) > 0);
   return tc.tdTurnOn(1);
 })
 .then(function (result) {
@@ -22,7 +28,6 @@ tc.tdGetNumberOfDevices()
 })
 .then(function (result) {
   console.log('device 1 was turned off:', result === 0);
-  return tc.tdTurnOff(1);
 })
 .catch(function (err) {
   console.error('and error occured', err);
--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+var consts = require('./lib/consts');
+
+exports.DeviceMethods = consts.DeviceMethods;
 exports.TelldusEvents = require('./lib/event');
 exports.TelldusClient = require('./lib/client');
